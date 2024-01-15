05eeedf5b498f2b9839d82b7ad89b14a0ea68124
Samuel <109113326+samuxka@users.noreply.github.com>
2024-01-15 19:17:23 -0300

    js2.o

---
+++ array/aula.js
@@ -0,0 +1,11 @@
+const notas = []
+
+notas.push(5)
+notas.push(7)
+notas.push(8)
+notas.push(2)
+notas.push(5)
+
+var soma = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) / notas.length
+
+console.log(soma.toFixed(1))
--- exercicios/exercicio07.js
+++ exercicios/exercicio07.js
@@ -12,8 +12,29 @@ class pessoa{
     calcimc(){
         return this.peso / (this.altura * this.altura)
     }
+
+    classimc(){
+        const imc = this.calcimc()
+
+        if (imc < 16.9) {
+            return "Cuidado!! Você está muito abaixo do peso"
+        } else if (imc >= 17 && imc <= 18.4) {
+            return "Você tem está abaixo do peso"
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            return "Você está no peso normal"
+        } else if (imc >= 25 && imc <= 29.9) {
+            return "Cuidado! Você está acima do peso"
+        } else if (imc >= 30 && imc <= 34.9) {
+            return "Cuidado!! Você está com obeside grau 1"
+        } else if (imc >= 35 && imc <= 40) {
+            return "Cuidado!! Você está com obeside grau 2"
+        } else {
+            return "Cuidado!! Você está com obeside grau 3"
+        }
+
+    }
 }
 
-const samuel = new pessoa('Samuel', 65, 1.70)
+const samuel = new pessoa('Samuel', 150, 1.70)
 console.log(samuel)
-console.log(samuel.calcimc)
+console.log(samuel.classimc())
